3acf7ffa62c9cf73541cc416c530da3fd6ba24a2
Ícaro <allegarcezz@gmail.com>
2021-08-15 22:08:18 -0300

    Criando model e classes de enrollment (task 2).

---
+++ src/main/java/br/com/alura/school/enrollment/EnrollmentController.java
@@ -0,0 +1,8 @@
+package br.com.alura.school.enrollment;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnrollmentController {
+    
+}
---
+++ src/main/java/br/com/alura/school/enrollment/EnrollmentRepository.java
@@ -0,0 +1,5 @@
+package br.com.alura.school.enrollment;
+
+public interface EnrollmentRepository {
+    
+}
